efd22a7219bb78b7087d0b242c99a3788437ef68
radio-cooperativa <wilmersoncco2017@gmail.com>
2026-05-29 08:25:11 -0500

    Add files via upload

---
+++ wp-admin/user/user-edit.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Edit user administration panel.
+ *
+ * @package WordPress
+ * @subpackage Administration
+ * @since 3.1.0
+ */
+
+/** Load WordPress Administration Bootstrap */
+require_once __DIR__ . '/admin.php';
+
+require ABSPATH . 'wp-admin/user-edit.php';
+<?php
+/**
+ * Edit user administration panel.
+ *
+ * @package WordPress
+ * @subpackage Administration
+ * @since 3.1.0
+ */
+
+/** Load WordPress Administration Bootstrap */
+require_once __DIR__ . '/admin.php';
+
+require ABSPATH . 'wp-admin/user-edit.php';
