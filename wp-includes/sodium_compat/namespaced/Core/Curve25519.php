a97daf3341db842bcdc6d895e0506cab9258764d
radio-cooperativa <wilmersoncco2017@gmail.com>
2026-05-29 09:05:08 -0500

    Add files via upload

---
+++ wp-includes/sodium_compat/namespaced/Core/Curve25519.php
@@ -0,0 +1,21 @@
+<?php
+namespace ParagonIE\Sodium\Core;
+
+class Curve25519 extends \ParagonIE_Sodium_Core_Curve25519
+{
+
+}
+<?php
+namespace ParagonIE\Sodium\Core;
+
+class Curve25519 extends \ParagonIE_Sodium_Core_Curve25519
+{
+
+}
+<?php
+namespace ParagonIE\Sodium\Core;
+
+class Curve25519 extends \ParagonIE_Sodium_Core_Curve25519
+{
+
+}
---
+++ wp-includes/sodium_compat/namespaced/Crypto.php
@@ -0,0 +1,14 @@
+<?php
+namespace ParagonIE\Sodium;
+
+class Crypto extends \ParagonIE_Sodium_Crypto
+{
+
+}
+<?php
+namespace ParagonIE\Sodium;
+
+class Crypto extends \ParagonIE_Sodium_Crypto
+{
+
+}
